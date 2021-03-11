30a9a16325c0be1968502e089ae2727a167c150c
Juan Romero <juancamilo1955@hotmail.com>
2021-03-11 15:32:03 +0100

    AtomeMenu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Atomes()
+    {
+        SceneManager.LoadScene("ScanAtomeindiv");
+    }
+    public void Molecule()
+    {
+        SceneManager.LoadScene("CompositionAtome");
+    }
+    public void Exercice()
+    {
+        SceneManager.LoadScene("EquilibreMoleculeExercice");
+    }
+    public void Aide()
+    {
+        SceneManager.LoadScene("Aide Tuto");
+    }
+}
